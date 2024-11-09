fd63cc40c2618950c47ca587f0e8d131e0b75981
xxyyffyeah <xxyyffyeah@126.com>
2024-11-09 01:49:28 -0600

    push

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout, 
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages/';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
+  },
+
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
