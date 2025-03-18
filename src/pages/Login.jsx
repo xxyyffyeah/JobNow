246ff84f546917b28234d8e559dae8470bed81de
xxyyffyeah <xxyyffyeah@126.com>
2025-03-18 14:13:55 -0500

    uiefhe

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,36 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo, FormRow } from '../components';
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to = '/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow
+          type="email"
+          name="email"
+          defaultValue="xxyyffyeah@outlook.com"
+        ></FormRow>
+        <FormRow
+          type="password"
+          name="password"
+          defaultValue="secret123"
+        ></FormRow>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Already a member?{' '}
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
-  
-}
-export default Login
+};
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { Logo, FormRow } from '../components';
 const Register = () => {
-    
   return (
     <Wrapper>
       <form className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type = "text" name='name' defaultValue='Charlie'/>
-        <button type="submit" className="btn btn-block">submit</button>
+        <FormRow type="text" name="name" defaultValue="Charlie" />
+        <FormRow type="text" name="lastName" labelText = 'Last Name' defaultValue="Xu" />
+        <FormRow type="text" name="location" defaultValue="earth" />
+        <FormRow type="email" name="email" defaultValue="xxyyffyeah@outlook.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
         <p>
-          Already a member? <Link to="/login" className="member-btn">Login</Link>
+          Already a member?{' '}
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
         </p>
       </form>
     </Wrapper>
   );
-}
-export default Register
+};
+export default Register;
